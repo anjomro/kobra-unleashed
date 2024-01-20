23911bcedf5d374e99553f92d8ee2c24a03c439b
anjomro <github@anjomro.de>
2024-01-20 16:00:49 +0100

    Add functionality to use socketio dev server for development

--- kobra-client/src/socket.js
+++ kobra-client/src/socket.js
@@ -1,30 +1,29 @@
-import { reactive } from 'vue';
-import { io } from 'socket.io-client';
+import {reactive} from 'vue';
+import {io} from 'socket.io-client';
 
 // Create a reactive state to store the Socket.IO connection status and events
 export const state = reactive({
-  connected: false,
-  events: []
+    connected: false,
+    events: []
 });
 
-// Create a Socket.IO connection to the Python Socket.IO service
-const socket = io(); // Replace with your Python Socket.IO service URL
+
+const socket = import.meta.env.PROD ? io() : io("http://localhost:5000");
 
 // Listen for the 'connect' event
 socket.on('connect', () => {
-  state.connected = true;
+    state.connected = true;
 });
 
 // Listen for the 'disconnect' event
 socket.on('disconnect', () => {
-  state.connected = false;
+    state.connected = false;
 });
 
 // Listen for a custom event, replace 'event_name' with your actual event name
 socket.on('event_name', (data) => {
-  state.events.push(data);
+    state.events.push(data);
 });
-
 
 
 // Export the socket so you can use it to emit events to the server
