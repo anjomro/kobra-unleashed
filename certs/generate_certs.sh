ff578e3b5bfeedbc38c593ad8ff6d8bd6ea9744f
Alexander <151234273+ultimateshadsform@users.noreply.github.com>
2024-02-11 06:26:02 +0100

    Update generate_certs.sh

--- certs/generate_certs.sh
+++ certs/generate_certs.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-#One line
-details="-subj /C=US/ST=CA/L=SanFrancisco/O=GlobalSecurity/OU=IT/CN=0.0.0.0"
+# Create mqtt certificates ca.crt, server.crt, server.key client.crt, client.key
 
 openssl genrsa -out ca.key 2048
-openssl req -new -x509 -days 3650 -key ca.key -out ca.crt $details
+openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/C=US/ST=CA/L=San Francisco/O=My Company/CN=ca"
 
 openssl genrsa -out server.key 2048
-openssl req -new -key server.key -out server.csr $details
+openssl req -new -out server.csr -key server.key -subj "/C=US/ST=CA/L=San Francisco/O=My Company/CN=localhost"
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650
+
 openssl genrsa -out client.key 2048
-openssl req -new -key client.key -out client.csr $details
+openssl req -new -out client.csr -key client.key -subj "/C=US/ST=CA/L=San Francisco/O=My Company/CN=client"
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 3650
 
-md5sum ca.crt server.crt server.key client.key client.crt
-
-# Gen scp
-echo "scp ca.crt server.crt server.key client.key client.crt root@10.0.2.249:/user"
+scp ca.crt server.crt server.key client.crt client.key root@10.0.2.249:/user
