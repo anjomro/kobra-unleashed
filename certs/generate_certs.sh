741c5ca211cbaece85c72bb52e46705a419d4fbd
anjomro <github@anjomro.de>
2024-02-12 17:18:53 +0100

    Make key world readable

--- certs/generate_certs.sh
+++ certs/generate_certs.sh
@@ -7,3 +7,6 @@ openssl genrsa -out ca.key 2048 && openssl req -x509 -new -nodes -key ca.key -sh
 openssl genrsa -out verification_cert.key 2048 && openssl req -new -key verification_cert.key -out verification_cert.csr && openssl x509 -req -in verification_cert.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out verification_cert.pem -days 2048 -sha256
 
 openssl genrsa -out client.key 2048 && openssl req -new -key client.key -out client.csr && openssl x509 -req -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.pem -days 500 -sha256
+
+#Make key world readable
+chmod 744 verification_cert.key
