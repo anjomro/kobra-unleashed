5f144c3137c3b91b7de2a1253e42bce6c68b18ca
anjomro <github@anjomro.de>
2024-03-14 23:47:44 +0100

    Change line endings for script back to unix, correct names

--- certs/generate_certs.sh
+++ certs/generate_certs.sh
@@ -7,15 +7,10 @@ openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/C=US/ST=CA/L=S
 
 openssl genrsa -out server.key 2048
 openssl req -new -out server.csr -key server.key -subj "/C=US/ST=CA/L=San Francisco/O=My Company/CN=localhost"
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.pem -days 3650
 
 openssl genrsa -out client.key 2048
 openssl req -new -out client.csr -key client.key -subj "/C=US/ST=CA/L=San Francisco/O=My Company/CN=client"
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 3650
-
-# Add hardlinks to make it work with mosquitto
-ln server.key verification_cert.key
-ln server.pem verification_cert.pem
-
 
 echo "Please copy ca.crt server.crt server.key client.crt client.key to your printer under /user/"
